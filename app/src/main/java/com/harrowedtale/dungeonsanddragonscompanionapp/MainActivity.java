fa1f408fe73fd6c81ff414e2a257eb19e7e36d6f
Trenton Blackburn <trent_blackburn97@yahoo.com>
2019-03-07 21:58:07 -0600

    added navigation bar and GridView for main menu

--- app/src/main/java/com/harrowedtale/dungeonsanddragonscompanionapp/MainActivity.java
+++ app/src/main/java/com/harrowedtale/dungeonsanddragonscompanionapp/MainActivity.java
@@ -1,13 +1,54 @@
 package com.harrowedtale.dungeonsanddragonscompanionapp;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+
+    String[] menuOptions = {
+            "Bestiary", "Items", "Spells",
+            "Locations", "Classes", "Rules",
+            "Feats", "Races", "Custom Items"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDrawerLayout = findViewById(R.id.drawerLayout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        ArrayAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, menuOptions);
+
+        GridView gridView = findViewById(R.id.menu_list);
+        gridView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
