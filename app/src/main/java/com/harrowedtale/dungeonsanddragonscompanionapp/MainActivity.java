0fe5817222364b15195919829da34210fd74fdaf
Adrian LaCour <adrianj.lacour@gmail.com>
2019-04-23 22:18:44 -0500

    Changed up the main page activity to make it more visually appealing

--- app/src/main/java/com/harrowedtale/dungeonsanddragonscompanionapp/MainActivity.java
+++ app/src/main/java/com/harrowedtale/dungeonsanddragonscompanionapp/MainActivity.java
@@ -6,16 +6,15 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.GridLayout;
 import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private DrawerLayout mDrawerLayout;
-    private ActionBarDrawerToggle mToggle;
 
     String[] menuOptions = {
             "Grimoire", "Custom Items",
@@ -23,23 +22,24 @@ public class MainActivity extends AppCompatActivity {
             "New User Wizard"
     };
 
+    GridLayout mainGrid;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mDrawerLayout = findViewById(R.id.drawerLayout);
-        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+        mainGrid = (GridLayout) findViewById(R.id.menu_list);
 
-        mDrawerLayout.addDrawerListener(mToggle);
-        mToggle.syncState();
+        //Set event
+        setSingleEvent(mainGrid);
 
         ActionBar actionBar = getSupportActionBar();
         if(actionBar != null){
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
 
-        ArrayAdapter adapter = new ArrayAdapter<>(this, R.layout.mainmenu_list_item, menuOptions);
+        /*ArrayAdapter adapter = new ArrayAdapter<>(this, R.layout.mainmenu_list_item, menuOptions);
 
         GridView gridView = findViewById(R.id.menu_list);
         gridView.setAdapter(adapter);
@@ -74,15 +74,48 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(newUserWizard);
                 }
             }
-        });
+        });*/
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if(mToggle.onOptionsItemSelected(item)){
-            return true;
+    private void setSingleEvent(GridLayout mainGrid){
+        //Loop all child items of Main Grid
+        for(int i = 0; i < mainGrid.getChildCount(); i++)
+        {
+            //All children are card views
+            CardView cardView = (CardView) mainGrid.getChildAt(i);
+            final int finalI = i;
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(finalI == 0)//For grimoire
+                    {
+                        Intent grimoire = new Intent(MainActivity.this, GrimoireActivity.class);
+                        startActivity(grimoire);
+                    }
+                    if(finalI == 1)//For custom items
+                    {
+                        //TODO add in the correct custom items class
+                        //Intent customItems = new Intent(MainActivity.this, customItems.class);
+                        //startActivity(customItems);
+                    }
+                    else if(finalI == 2)//For the dice roller
+                    {
+                        Intent diceRoller = new Intent(MainActivity.this, DiceActivity.class);
+                        startActivity(diceRoller);
+                    }
+                    else if(finalI == 3)//For the character sheets roller
+                    {
+                        Intent characterSheet = new Intent(MainActivity.this, CharacterSheet.class);
+                        startActivity(characterSheet);
+                    }
+                    else if(finalI == 4)//For the new character creation wizard roller
+                    {
+                        Intent newUserWizard = new Intent(MainActivity.this, NewUserWizard.class);
+                        startActivity(newUserWizard);
+                    }
+                }
+            });
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
 }
