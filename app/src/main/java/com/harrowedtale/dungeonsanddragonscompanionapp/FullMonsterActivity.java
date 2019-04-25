cc8680543c72bd1bd0e32100ab960cccaf55b4de
WigginsDamien <31625833+WigginsDamien@users.noreply.github.com>
2019-04-24 20:31:32 -0500

    Updated individual monster layout.

--- app/src/main/java/com/harrowedtale/dungeonsanddragonscompanionapp/FullMonsterActivity.java
+++ app/src/main/java/com/harrowedtale/dungeonsanddragonscompanionapp/FullMonsterActivity.java
@@ -13,12 +13,26 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 import java.util.Map;
 
 public class FullMonsterActivity extends AppCompatActivity {
 
     public TextView monsterdata;
+    public TextView intelligence;
+    public TextView strength;
+    private TextView charisma;
+    private TextView dexterity;
+    private TextView wisdom;
+    private TextView constitution;
+    private TextView monsterActions;
+    private TextView monsterLegendaryActions;
+    private TextView monsterAbilities;
+    private TextView monsterLegendaryActionTitle;
+    private TextView monsterAbilityTitle;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     CollectionReference Monster = db.collection("Monsters");
 
@@ -26,23 +40,34 @@ public class FullMonsterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.monster_full);
+        monsterLegendaryActionTitle = findViewById(R.id.monsterLegendaryActionsTitle);
+        monsterAbilityTitle = findViewById(R.id.monsterAbilityTitle);
         monsterdata = findViewById(R.id.monsterData);
+        intelligence = findViewById(R.id.monsterInt);
+        strength = findViewById(R.id.monsterStr);
+        charisma = findViewById(R.id.monsterCha);
+        dexterity = findViewById(R.id.monsterDex);
+        wisdom = findViewById(R.id.monsterWis);
+        constitution = findViewById(R.id.monsterCon);
+        monsterActions = findViewById(R.id.monsterActionData);
+        monsterLegendaryActions = findViewById(R.id.monsterLegendaryActions);
+        monsterAbilities = findViewById(R.id.monsterAbility);
         Intent monster_display = getIntent();
-        String Monster_name = monster_display.getStringExtra("Name");
+        String Monster_name = monster_display.getStringExtra("Name");//get monster name from previous activity
         setTitle(Monster_name);//Sets page title to the respective monster
-        DocumentReference monster = db.collection("Monsters").document(Monster_name);
+        DocumentReference monster = db.collection("Monsters").document(Monster_name);//open document in the database
  //       Toast.makeText(this, Monster_name, Toast.LENGTH_SHORT).show();
         monster.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                String monsterinfo = "Stats:\n\n";
-                DocumentSnapshot snap = task.getResult();
+                String monsterinfo = "";
+                DocumentSnapshot snap = task.getResult();//assigns the document to snap
                 String temp;
                 Number temp_int;
                 ArrayList<Map<String,String>> test = (ArrayList<Map<String, String>>) snap.get("actions");
                 if(snap.contains("hitPoints")) {
                     temp_int = (Number) snap.get("hitPoints");
-                    monsterinfo += ("Hitpoints: " + temp_int + "\n");
+                    monsterinfo = ("Hitpoints: " + temp_int + "\n");
                 }
                 if(snap.contains("hitDice")) {
                     temp = (String) snap.get("hitDice");
@@ -58,23 +83,26 @@ public class FullMonsterActivity extends AppCompatActivity {
                 }
                 if(snap.contains("constitution")) {
                     temp_int = (Number) snap.get("constitution");
-                    monsterinfo += ("Constitution: " + String.valueOf(temp_int) + "\n");
+                    constitution.setText(String.valueOf(temp_int));
                 }
                 if(snap.contains("dexterity")) {
                     temp_int = (Number) snap.get("dexterity");
-                    monsterinfo += ("Dexterity: " + String.valueOf(temp_int) + "\n");
+                    dexterity.setText(String.valueOf(temp_int));
                 }
                 if(snap.contains("intelligence")) {
                     temp_int = (Number) snap.get("intelligence");
-                    monsterinfo += ("Intelligence: " + String.valueOf(temp_int) + "\n");
+                    intelligence.setText(String.valueOf(temp_int));
                 }
                 if(snap.contains("strength")) {
                     temp_int = (Number) snap.get("strength");
-                    monsterinfo += ("Strength: " + String.valueOf(temp_int) + "\n");
+                    strength.setText(String.valueOf(temp_int));
                 }
                 if(snap.contains("wisdom")) {
                     temp_int = (Number) snap.get("wisdom");
-                    monsterinfo += ("Wisdom: " + String.valueOf(temp_int) + "\n");
+                    wisdom.setText(String.valueOf(temp_int));
+                }
+                if(snap.contains("charisma")) {
+                    charisma.setText(String.valueOf(snap.get("charisma")));
                 }
                 if(snap.contains("alignment")) {
                     temp = (String) snap.get("alignment");
@@ -121,43 +149,49 @@ public class FullMonsterActivity extends AppCompatActivity {
                     monsterinfo += ("Senses: " + temp + "\n\n");
                 }
                 //String monster_alignment = monster_for_page.getAlignment();
+                String monster_actions = "";
                 if(snap.get("actions") != "") {
-                    monsterinfo += ("Actions:\n\n");
                     for (Map<String, String> num : test) {
-                        monsterinfo += num.get("name");
-                        monsterinfo += ":\n";
-                        monsterinfo += num.get("description");
-                        monsterinfo += "\n\n";
+                        monster_actions += num.get("name");
+                        monster_actions += ":\n";
+                        monster_actions += num.get("description");
+                        monster_actions += "\n\n";
                     }
                 }
+                monsterActions.setText(monster_actions);
                 if(snap.get("legendaryActions") != "") {
                     test = (ArrayList<Map<String, String>>) snap.get("legendaryActions");
                     if(test.isEmpty()) {
 
                     }
                     else {
-                        monsterinfo += ("Legendary Actions:\n\n");
+                        String monsterlegendaryactions = "";
+                        String title = "Legendary Actions";
+                        monsterLegendaryActionTitle.setText(title);
                         for (Map<String, String> num : test) {
-                            monsterinfo += num.get("name");
-                            monsterinfo += ":\n";
-                            monsterinfo += num.get("description");
-                            monsterinfo += "\n\n";
+                            monsterlegendaryactions += num.get("name");
+                            monsterlegendaryactions += ":\n";
+                            monsterlegendaryactions += num.get("description");
+                            monsterlegendaryactions += "\n\n";
                         }
+                        monsterLegendaryActions.setText(monsterlegendaryactions);
                     }
                 }
                 if(snap.get("specialAbilities") != "") {
                     test = (ArrayList<Map<String, String>>) snap.get("specialAbilities");
                     if(test.isEmpty()) {
-
                     }
                     else {
-                        monsterinfo += ("Special Abilities:\n\n");
+                        String title = ("Special Abilities");
+                        String monsterabilities = "";
+                        monsterAbilityTitle.setText(title);
                         for (Map<String, String> num : test) {
-                            monsterinfo += num.get("name");
-                            monsterinfo += ":\n";
-                            monsterinfo += num.get("description");
-                            monsterinfo += "\n\n";
+                            monsterabilities += num.get("name");
+                            monsterabilities += ":\n";
+                            monsterabilities += num.get("description");
+                            monsterabilities += "\n\n";
                         }
+                        monsterAbilities.setText(monsterabilities);
                     }
                 }
                 monsterdata.setText(monsterinfo);
